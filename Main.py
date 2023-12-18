cbf53f57a8ad9c96b7d4f42c1f864945c6abd474
Anonyyymous <78022284+Anonyyymous@users.noreply.github.com>
2023-12-18 07:47:31 +0000

    Cleaned Main.py
    Tidied up some code, organised it/labelled how i've organised the if elses because yes, they were organised before. Also added some comments

--- Main.py
+++ Main.py
@@ -8,23 +8,29 @@ from HungerGamesClasses import Player, GameManager, clean_input_for_games
 from TimetableChecker import get_day
 
 # im going to regret this
-tokenPath = "/home/oreo/Desktop/DiscordBot/token.txt"
+tokenPath = "/home/oreo/Desktop/DiscordBot/token.txt"  # could at some point replace with an if statement, so this works on my pc as well without replacing the code with the token
+# or, could use os.path.dirname, some splits, and store it in the same place relative to those directions on each machine
+
+# reading token
 file = open(tokenPath, "r")
 token = file.readline()
 file.close()
 
+# initialising bot
 intents = discord.Intents(messages=True, guilds=True, message_content=True)
 client = discord.Client(intents=intents)
+
+# initialising other variables
 ids = {"john": 331837038024327182, "lauren": 423146074233110528, "isaac": 386783880637579286}
 message_count = 0
 playing_hunger_games = False
 ash_count = 0
 game_manager = None
 
-stored_file_name = os.path.join(os.path.dirname(os.path.abspath(__file__)), "NamesCount.txt")
+stored_file_name = os.path.join(os.path.dirname(os.path.abspath(__file__)), "NamesCount.txt")  # i think just "NamesCount.txt" should work, im not sure what prompted this
 
 @client.event
-async def on_ready():
+async def on_ready():  # executed on bot setup
     print("logged in as {0.user}".format(client))
     get_names()
     # await client.user.edit(username="John's assistant")
@@ -36,37 +42,45 @@ async def on_message(message):
     
     global ash_count, message_count, playing_hunger_games, game_manager
     message_count += 1
+    message_contents_full = message.content.lower()
+    message_contents = message.content.lower().strip()
+    
     print(message.author, ": ", message.content)
 
-    if message.content == "-updatebot":
+    if message_contents == "-updatebot":
+        '''
+        (explain whats going one here, once it works)
+        '''
         g = git.cmd.Git("/home/oreo/Desktop/DiscordBot/DiscordBotTest")
         g.pull()
         #subprocess.Popen(["/bin/bash", "./update.sh"], shell=True)
         #exit()
         os.execv(__file__,sys.argv)
+        return
 
     rand_num = random.randrange(0, 500)
+
+    # random responses
     if rand_num == 69:
         await message.channel.send("kill yourself - wiktor")
 
-    if message_count == 15:
+    if message_count == 15:  # not necessary now, might remove, or make it random
         message_count = 0
-        save_names(["ash", "sofia"], [ash_count, sofia_count])
+        save_names(["ash"], [ash_count])
         print("names saved")
     
     FuF = ["fucku", "fuckyou"]
 
-    message_contents_full = message.content.lower()
-    message_contents = message.content.lower().strip()
+    # responses
     if message_contents in FuF:
         await message.channel.send("not if i fuck you first")
         print("successfully fucked")
     elif message_contents == "kys":
         await message.channel.send("awww keep yourself safe so sweet")
-
     elif "good morning" in message_contents:
         await message.channel.send("go die")
 
+    # activities
     elif "-hungergames" == message_contents[:12]:
         if playing_hunger_games:
             # -hungergames|john|isaac|lauren|wiktor|george|kerry|biggie dikkie mikkie|matteo|filip
@@ -76,17 +90,19 @@ async def on_message(message):
             playing_hunger_games = True
             game_manager = GameManager(clean_input_for_games(message_contents))
             await message.channel.send(game_manager.play_round()[0])
-    elif "-timetable" == message_contents[:10]:
+    elif "-timetable" == message_contents:
         await message.channel.send(get_day())
 
-    elif message.author.id == ids["isaac"] and "ash" in message_contents_full:  # because he could say 'a sheep' for example
+    # id specific messages
+    elif message.author.id == ids["isaac"] and "ash" in message_contents_full:  # using message_contens_full, because he could say 'a sheep' for example
         ash_count+=1
         await message.channel.send("such a fucking simp")
-    elif message.author.id != ids["lauren"] and message_contents == "-summon isaac":
+    elif message.author.id != ids["lauren"] and message_contents == "-summon isaac":  # heheheha
         print("summoning isaac")
         await message.channel.send("<@386783880637579286> <@386783880637579286> <@386783880637579286> <@386783880637579286> <@386783880637579286>")
 
-    elif message_contents == "-update":
+    # displaying counters
+    elif message_contents == "-update":  # this could be removed
         message_count = 0
         save_names(["ash"], [ash_count])
         print("names saved")
@@ -95,11 +111,10 @@ async def on_message(message):
 
 def get_names():
     if os.path.exists(stored_file_name):
-        global sofia_count, ash_count
+        global ash_count
         with open(stored_file_name, "r") as f:
             lines = f.readlines()
             ash_count = int(lines[0].split("|")[1])
-            sofia_count = int(lines[1].split("|")[1])
             print("read file")
             print(f"names = {ash_count}")
     else:
