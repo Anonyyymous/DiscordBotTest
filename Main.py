d39fc02a782daeb1f434364f4595f4e6063d4a0d
Anonyyymous <78022284+Anonyyymous@users.noreply.github.com>
2023-12-19 21:10:42 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -87,10 +87,10 @@ async def on_message(message):
             await send(message.channel, get_day())
         elif "-roulette" == message_contents:
             member_id = random.choice(message.guild.members).id
-            temp = client.get_guild(message.channel.guild.id)
-            await log(f"members: {message.guild.members}, {temp}, {temp.members}")
-            str1 = ''.join(f"<@{member_id}> " for i in range(10))
-            await send(message.channel, str1)  # im being generous
+            # temp = client.get_guild(message.channel.guild.id)
+            # await log(f"members: {message.guild.members}, {temp}, {temp.members}")
+            # str1 = ''.join(f"<@{member_id}> " for i in range(10))
+            await send(message.channel, ''.join(f"<@{member_id}> " for i in range(10)))  # im being generous
         elif message_contents == "-counters":
             phraseCountFile = open(os.path.join(os.getcwd(),"local/phraseCounters.txt"), "r")
             currentCounts = phraseCountFile.readlines()
