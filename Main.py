ea5480f85cd00e30b2af10dfefa36007fbe1de1e
Anonyyymous <78022284+Anonyyymous@users.noreply.github.com>
2023-12-19 20:59:14 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -102,7 +102,7 @@ async def on_message(message):
     
             await send(message.channel, text)
     except Exception as e:
-        await send(message.channel, "an error occured:", e)
+        await send(message.channel, f"an error occured: {e}")
 
 async def HandleResponses(message):
     for i in range(len(responses)):
