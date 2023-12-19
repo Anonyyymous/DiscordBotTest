49a9e80136ee4d5b32fb204e825f175d21611210
OreoRedstone <isaac.the.owen@gmail.com>
2023-12-19 13:32:26 +0000

    Got counters working.

--- Main.py
+++ Main.py
@@ -1,13 +1,14 @@
 import discord
 import os
+import regex
 import random
 from HungerGamesClasses import Player, GameManager, clean_input_for_games
 from TimetableChecker import get_day
 import json
 
-# im going to regret this
-tokenPath = "/home/oreo/Desktop/DiscordBot/token.txt"  # could at some point replace with an if statement, so this works on my pc as well without replacing the code with the token
-# or, could use os.path.dirname, some splits, and store it in the same place relative to those directions on each machine
+thisDir = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+tokenPath = os.path.join(os.getcwd(), "local/token.txt")
 
 # reading token
 file = open(tokenPath, "r")
@@ -23,23 +24,16 @@ ids = {"john": 331837038024327182, "lauren": 423146074233110528, "isaac": 386783
 playing_hunger_games = False
 game_manager = None
 
-thisDir = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
 responsesFile = open(os.path.join(thisDir, "responses.txt"), "r")
 responses = responsesFile.readlines()
 responsesFile.close()
-
-counters = {}
-with open('names_count.json', 'r') as f:
-    # Reading from json file
-    counters = json.load(f)
 
 @client.event
 async def on_ready():  # executed on bot setup
     print("logged in as {0.user}".format(client))
     #get_names()
     await client.wait_until_ready()
-    channel = client.get_channel(1181162678740324392)
+    channel = client.get_channel(1186638026781249606)
     await channel.send(client.user.display_name + " up and running.")
     # await client.user.edit(username="John's assistant")
 
@@ -49,16 +43,10 @@ async def send(channel, text):  # might need to @client.event wrapper but dont t
         return
     await channel.send(text)
 
-
 @client.event
 async def on_message(message):
     if message.author == client.user:  # for bot-free zone, could be loaded into a json later for bot-free channels
         return
-    
-    '''async def send(text):  # might need to @client.event wrapper but dont think so. If this doesnt work, just replace it for now
-        if message.channel.id == 1186331140030746764:  # if channel.id in bot_free_channels
-            return
-        await message.channel.send(random.choice(text))'''
     
     global playing_hunger_games, game_manager, counters
     message_contents_full = message.content.lower()
@@ -71,49 +59,9 @@ async def on_message(message):
         await client.close()
         exit()
 
-    for i in range(len(responses)):
-        response = responses[i].split('<>')
-        words = response[0].split('__')
-        for j in range(len(words)):
-            if words[j] in message_contents_full:  # using full so it ignores spaces for yu huh, etc
-                if str(message.author.id) in response[1].split('__') or response[1] == "all":
-                    if random.random() < float(response[2]):
-                        answers = response[3].split('__')
-                        await send(message.channel, random.choice(answers))
+    await HandleResponses(message)
 
-    '''namesCountFile = open(os.path.join(thisDir, "NamesCount.txt"), "r")
-    nameCounters = namesCountFile.readlines()
-    namesCountFile.close()
-
-    for i in range(len(nameCounters)):
-        try:
-            currentNameCounter = nameCounters[i].replace('\n', '')
-            if currentNameCounter == "":
-                continue
-            identifier = currentNameCounter.split('|')[0]
-            if identifier.split('-')[1] in message_contents and identifier.split('-')[0] == str(message.author.id):
-                print(identifier.split('-')[0] + " said " + identifier.split('-')[1])
-                nameCounters[i] = nameCounters[i].split('|')[0] + "|" + str(int(nameCounters[i].split('|')[1]) + message_contents.count(identifier.split('-')[1]))
-        except:
-            print("ERROR IDK")'''
-    for key in counters.keys():
-        if ids[key] == message.author.id:  # use actual names for more readability
-            for keyword in counters[key].keys():
-                if keyword in message_contents:
-                    counters[key][keyword] += 1
-
-
-    '''namesCountText = ""
-    for i in range(len(nameCounters) - 1):
-        namesCountText += str(nameCounters[i].replace('\n', '')) + "\n"
-    namesCountText += str(nameCounters[-1])
-
-    namesCountFile = open(os.path.join(thisDir, "NamesCount.txt"), "w")
-    namesCountFile.write(namesCountText)
-    namesCountFile.close()'''
-    with open('names_count.json', 'w') as f:
-        json.dump(counters, f)
-
+    await HandleCounters(message)
     
     rand_num = random.randrange(0, 500)
 
@@ -134,16 +82,72 @@ async def on_message(message):
     elif "-timetable" == message_contents:
         await send(message.channel, get_day())
     elif message_contents == "-counters":
-        '''namesCountFile = open(os.path.join(thisDir, "NamesCount.txt"), "r")
-        nameCounters = namesCountFile.readlines()
-        namesCountFile.close()
+        phraseCountFile = open(os.path.join(os.getcwd(),"local/phraseCounters.txt"), "r")
+        currentCounts = phraseCountFile.readlines()
+        phraseCountFile.close()
         
-        for i in range(len(nameCounters)):
-            text += "<@" + nameCounters[i].split('|')[0].split('-')[0] + "> " + nameCounters[i].split('|')[0].split('-')[1] + " count: " + nameCounters[i].split('|')[1]'''
         text = ""
-        for key in counters:
-            for keyword in counters[key]:
-                text += f"{key}|{keyword}:{counters[key][keyword]}\n"  # also doesnt @ them
+        
+        for phrase in currentCounts:
+            text += GetName(phrase.split('|')[0].split('-')[0]) + " " + phrase.split('|')[0].split('-')[1] + " count: " + phrase.split('|')[1]
+
         await send(message.channel, text)
+
+async def HandleResponses(message):
+    for i in range(len(responses)):
+        response = responses[i].split('<>')
+        words = response[0].split('__')
+        for j in range(len(words)):
+            if words[j] in message.content.lower():  # using full so it ignores spaces for yu huh, etc
+                if str(message.author.id) in response[1].split('__') or response[1] == "all":
+                    if random.random() < float(response[2]):
+                        answers = response[3].split('__')
+                        await send(message.channel, random.choice(answers))
+
+async def HandleCounters(message):
+    phraseCounterDefinitions = open(os.path.join(os.getcwd(), "phraseCounterDefinitions.txt"), "r")
+    phraseCounters = phraseCounterDefinitions.readlines()
+    phraseCounterDefinitions.close()
+    
+    phraseCountFile = open(os.path.join(os.getcwd(),"local/phraseCounters.txt"), "r")
+    currentCounts = phraseCountFile.read()
+    phraseCountFile.close()
+    
+    text = ""
+    
+    for phrase in phraseCounters:
+        phrase = phrase.strip().replace("\n", "")
+        if phrase == "":
+            continue
+        if phrase.split('-')[1] in message.content.lower() and phrase.split('-')[0] == str(message.author.id):
+            #print(phrase.split('-')[0] + " said " + phrase.split('-')[1])
+            pattern = phrase + "\|(.+)\n"
+            match = regex.search(pattern, currentCounts)
+            if bool(match):
+                text += phrase + "|" + str(message.content.lower().count(phrase.split('-')[1]) + int(match.group(1))) + "\n"
+            else:
+                text += phrase + "|" + str(message.content.lower().count(phrase.split('-')[1])) + "\n"
+        else:
+            try:
+                pattern = phrase + "\|(.+)\n"
+                match = regex.search(pattern, currentCounts)
+                text += match.group(0)
+            except:
+                text += phrase + "|0\n"
+               
+    phraseCountFile = open(os.path.join(os.getcwd(),"local/phraseCounters.txt"), "w") 
+    phraseCountFile.write(text)
+    phraseCountFile.close()
+
+def GetName(id):
+    file = open(os.path.join(os.getcwd(), "idNameConversion.txt"))
+    text = file.read()
+    file.close()
+    
+    pattern = str(id) + "-(.+)\n"
+    #print(pattern)
+    #print(text)
+    match = regex.search(pattern, text)
+    return match.group(1)
 
 client.run(token)
